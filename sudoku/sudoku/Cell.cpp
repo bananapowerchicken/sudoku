07f4063e2bb80f20f3f1db6307d150e59fe04c13
bananapowerchicken <221882@mail.ru>
2021-10-27 22:56:13 +0300

    need to fix square check

--- sudoku/sudoku/Cell.cpp
+++ sudoku/sudoku/Cell.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
 #include <time.h>
 
+// неполное покрытие
 bool Cell::isSameSquare(int row, int col)
 {
+	if (row < 0 || col < 0)
+		return false;
+
 	if (std::abs(row - m_row) > 2 || std::abs(col - m_col) > 2)
 		return false;
 	return true; 
@@ -12,22 +16,62 @@ bool Cell::isSameSquare(int row, int col)
 // rand() % (b - a + 1) + a
 int Cell::setValue()
 {
-	srand(time(NULL)); // for truly randomizing
 	int ind = rand() % m_possibleVals.size(); // в диапазоне длины вектора (которая уменьшается)
-	m_value = m_possibleVals[ind];
-	std::cout << "set value = " << m_value << " with random index = " << ind << "\n";
+	m_value = m_possibleVals[ind];	
+	//std::cout << "set value = " << m_value << " with random index = " << ind << "\n";
 
 	return ind; // index to delete for other cells
 }
 
-int Cell::deletePossibleVal(int ind)
+// что за индекс?
+int Cell::deletePossibleVal(int val)
 {
-	m_possibleVals.erase(m_possibleVals.begin() + ind);
+	std::vector<int>::iterator it = std::find(m_possibleVals.begin(), m_possibleVals.end(), val);
+	if (it != m_possibleVals.end())
+		m_possibleVals.erase(it);
 
-	std::cout << "myvector contains:";
-	for (unsigned i = 0; i < m_possibleVals.size(); ++i)
-		std::cout << ' ' << m_possibleVals[i];
-	std::cout << '\n';
+	//std::cout << "vector size = " << m_possibleVals.size() << "\n";
+	//std::cout << "i = " << m_row << " j = " << m_col << "\n";
+	//std::cout << "myvector contains:";
+	//for (unsigned i = 0; i < m_possibleVals.size(); ++i)
+	//	std::cout << ' ' << m_possibleVals[i];
+	//std::cout << '\n';
 
 	return 0;
+}
+
+int Cell::findMaxSquareCoords()
+{
+	// i
+	if (this->isSameSquare(m_row - 2, m_col))
+		m_max_row = m_row;
+	else if (this->isSameSquare(m_row + 2, m_col))
+		m_max_row = m_row + 2;
+	else
+		m_max_row = m_row + 1;
+
+	// j
+	if (this->isSameSquare(m_row, m_col - 2))
+		m_max_col = m_col;
+	else if (this->isSameSquare(m_row, m_col + 2))
+		m_max_col = m_col + 2;
+	else
+		m_max_col = m_col + 1;
+
+
+
+	return 0;
+}
+
+bool Cell::init(int i, int j)
+{
+	// задает координаты
+	// вычисляет максим координаты квадрата, чтобы в дальнейшем исключать лишние возможные значения в квадрате
+
+	setRow(i);
+	setCol(j);	
+
+	findMaxSquareCoords();
+	
+	return true;
 }
--- sudoku/sudoku/Cell.h
+++ sudoku/sudoku/Cell.h
@@ -1,18 +1,28 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
 class Cell 
 {
 private:
 	int m_row;
 	int m_col;
-	int m_value;
+	int m_value = 0;
+	int m_max_row;
+	int m_max_col;
 	std::vector<int> m_possibleVals = {1, 2, 3, 4, 5, 6, 7, 8, 9};// должен быть еще массив либо вектор либо список доступных значений
-	int deletePossibleVal(int val);
-
-public:
+	int findMaxSquareCoords();
 	void setRow(int row) { m_row = row; };
 	void setCol(int col) { m_col = col; };
-	bool isSameSquare(int r, int c);
+	bool isSameSquare(int r, int c);	
+
+public:
+	int getValue() { return m_value; };
+	int deletePossibleVal(int val);
+	bool init(int i, int j);
 	int setValue(); // sets random value
+	int getVecLen() { return m_possibleVals.size(); }
+	int getMaxRow() { return m_max_row; };
+	int getMaxCol() { return m_max_col; };
+	
 };
