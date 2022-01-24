d71c121f276608ae3582ddd85ed2b78391584e0a
bananapowerchicken <221882@mail.ru>
2022-01-24 22:38:49 +0300

    fix base matrix (make right shifts)

--- sudoku/sudoku/Grid.cpp
+++ sudoku/sudoku/Grid.cpp
@@ -29,14 +29,14 @@ void Grid::init()
 		
 	}	
 
-	m_rightShiftRow(1, 3);
-	m_rightShiftRow(2, 6);
+	m_rightShiftRow(1, 6);
+	m_rightShiftRow(2, 3);
 	m_rightShiftRow(3, 8);
-	m_rightShiftRow(4, 2);
-	m_rightShiftRow(5, 5);
+	m_rightShiftRow(4, 5);
+	m_rightShiftRow(5, 2);
 	m_rightShiftRow(6, 7);
-	m_rightShiftRow(7, 1);
-	m_rightShiftRow(8, 4);
+	m_rightShiftRow(7, 4);
+	m_rightShiftRow(8, 1);
 }
 
 void Grid::show()
